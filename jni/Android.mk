3afd6229104b3616469ff64bbd225c57ffefde52
Anton Alipov <alipov@ieee.org>
2019-05-30 22:10:59 +0300

    Refactoring: change package name

--- jni/Android.mk
+++ jni/Android.mk
@@ -6,6 +6,6 @@ LOCAL_MODULE     := native-utils
 LOCAL_C_INCLUDES := $(JNI_DIR)/utils/
 LOCAL_CFLAGS     += -Wall
 
-LOCAL_SRC_FILES := $(JNI_DIR)/utils/org_thoughtcrime_securesms_util_FileUtils.cpp
+LOCAL_SRC_FILES := $(JNI_DIR)/utils/su_sres_securesms_util_FileUtils.cpp
 
 include $(BUILD_SHARED_LIBRARY)
