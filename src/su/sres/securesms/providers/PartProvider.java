c0b56d714c9b74a90c449f7b53e0b9d25b2f7e9a
Anton Alipov <alipov@ieee.org>
2019-06-09 22:22:49 +0300

    Refactoring: refinements. Original versioning back.

--- src/su/sres/securesms/providers/PartProvider.java
+++ src/su/sres/securesms/providers/PartProvider.java
@@ -46,7 +46,7 @@ public class PartProvider extends ContentProvider {
 
   private static final String TAG = PartProvider.class.getSimpleName();
 
-  private static final String CONTENT_URI_STRING = "content://org.thoughtcrime.provider.securesms/part";
+  private static final String CONTENT_URI_STRING = "content://su.sres.provider.securesms/part";
   private static final Uri    CONTENT_URI        = Uri.parse(CONTENT_URI_STRING);
   private static final int    SINGLE_ROW         = 1;
 
@@ -54,7 +54,7 @@ public class PartProvider extends ContentProvider {
 
   static {
     uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
-    uriMatcher.addURI("org.thoughtcrime.provider.securesms", "part/*/#", SINGLE_ROW);
+    uriMatcher.addURI("su.sres.provider.securesms", "part/*/#", SINGLE_ROW);
   }
 
   @Override
