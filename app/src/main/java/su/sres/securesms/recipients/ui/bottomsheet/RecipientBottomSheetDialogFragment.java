89278f6a47aa4a6f76c2fe2ec09fc3e211648520
Anton Alipov <alipov@ieee.org>
2020-08-21 19:38:35 +0300

    Resources fix

--- app/src/main/java/su/sres/securesms/recipients/ui/bottomsheet/RecipientBottomSheetDialogFragment.java
+++ app/src/main/java/su/sres/securesms/recipients/ui/bottomsheet/RecipientBottomSheetDialogFragment.java
@@ -78,18 +78,18 @@ public final class RecipientBottomSheetDialogFragment extends BottomSheetDialogF
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.recipient_bottom_sheet, container, false);
 
-        avatar                 = view.findViewById(R.id.recipient_avatar);
-        fullName               = view.findViewById(R.id.full_name);
-        usernameNumber         = view.findViewById(R.id.username_number);
-        messageButton          = view.findViewById(R.id.message_button);
-        secureCallButton       = view.findViewById(R.id.secure_call_button);
-        blockButton            = view.findViewById(R.id.block_button);
-        unblockButton          = view.findViewById(R.id.unblock_button);
-        viewSafetyNumberButton = view.findViewById(R.id.view_safety_number_button);
-        makeGroupAdminButton   = view.findViewById(R.id.make_group_admin_button);
-        removeAdminButton      = view.findViewById(R.id.remove_group_admin_button);
-        removeFromGroupButton  = view.findViewById(R.id.remove_from_group_button);
-        adminActionBusy        = view.findViewById(R.id.admin_action_busy);
+        avatar                 = view.findViewById(R.id.rbs_recipient_avatar);
+        fullName               = view.findViewById(R.id.rbs_full_name);
+        usernameNumber         = view.findViewById(R.id.rbs_username_number);
+        messageButton          = view.findViewById(R.id.rbs_message_button);
+        secureCallButton       = view.findViewById(R.id.rbs_secure_call_button);
+        blockButton            = view.findViewById(R.id.rbs_block_button);
+        unblockButton          = view.findViewById(R.id.rbs_unblock_button);
+        viewSafetyNumberButton = view.findViewById(R.id.rbs_view_safety_number_button);
+        makeGroupAdminButton   = view.findViewById(R.id.rbs_make_group_admin_button);
+        removeAdminButton      = view.findViewById(R.id.rbs_remove_group_admin_button);
+        removeFromGroupButton  = view.findViewById(R.id.rbs_remove_from_group_button);
+        adminActionBusy        = view.findViewById(R.id.rbs_admin_action_busy);
 
         return view;
     }
